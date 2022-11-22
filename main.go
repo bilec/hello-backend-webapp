f3ff06bf0dc90a5966457ca9ad65cf1fc6de3d1b
bilec <martin.bilka826@gmail.com>
2022-11-22 18:14:41 +0100

    feat: web server

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var port = "8080"
+
+func init() {
+	var envPort = os.Getenv("PORT")
+	if envPort != "" {
+		port = envPort
+	}
+}
 
 func main() {
-	log.Println("Hello from backend")
+	log.Println("Starting hello-backend-webapp on port " + port + " ...")
+
+	http.HandleFunc("/backend", backedHandler)
+
+	server := http.Server{Addr: ":" + port}
+	go func() {
+		log.Fatal(server.ListenAndServe())
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+
+	log.Println("Shutdown signal received, exiting...")
+
+	server.Shutdown(context.Background())
+}
+
+func backedHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from backend"))
 }
